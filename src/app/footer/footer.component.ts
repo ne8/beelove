5219ba3bda9cbe29390113b7c2a178986092a13b
neop <cpatrut@outlook.com>
2019-02-09 23:48:15 +0200

    Adding social media icons

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -7,6 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
   facebookUrl = 'https://www.facebook.com/stuco.ro/';
+  instagramUrl = 'https://www.instagram.com/stucostuco/';
+  twitterUrl = 'https://twitter.com/StucoStuco1';
+  snapachtUrL = 'tel://+40731898657';
+  whatsappUrl = 'tel://+40731898657';
+  phoneNumber = '+40731898657';
+  emailAddress = 'none';
+  // tumblrUrl = 'https://www.tumblr.com/search/stuco1234';
+  // youtubeUrl = 'youtube.com';
+  // pinterestUrl = 'pintereset.com';
+  companyName = 'Stuco';
   constructor() {}
 
   ngOnInit() {}
